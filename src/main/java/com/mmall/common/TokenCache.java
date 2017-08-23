cf322e3382f587d09e81881f478002e2d329a6ac
imooc <xianhongle95@163.com>
2017-08-23 23:31:31 +0800

    shipping module commit

--- src/main/java/com/mmall/common/TokenCache.java
+++ src/main/java/com/mmall/common/TokenCache.java
@@ -43,4 +43,5 @@ public class TokenCache {
         }
         return null;
     }
+
 }
---
+++ src/main/java/com/mmall/controller/portal/ShippingController.java
@@ -0,0 +1,82 @@
+package com.mmall.controller.portal;
+
+import com.mmall.common.Const;
+import com.mmall.common.ResponseCode;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Shipping;
+import com.mmall.pojo.User;
+import com.mmall.service.IShippingService;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+
+/**
+ * Created by 81975 on 2017/8/23.
+ */
+@Controller
+@RequestMapping("/shipping/")
+public class ShippingController {
+
+
+    @Autowired
+    private IShippingService iShippingService;
+
+
+    @RequestMapping("add.do")
+    @ResponseBody
+    public ServerResponse<Map> add(HttpSession session, Shipping shipping){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if (user==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.add(user.getId(),shipping);
+    }
+
+
+    @RequestMapping("delete.do")
+    @ResponseBody
+    public ServerResponse delete(HttpSession session,Integer shippingId){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if (user==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.delete(shippingId,user.getId());
+    }
+
+    @RequestMapping("update.do")
+    @ResponseBody
+    public ServerResponse update(HttpSession session,Shipping shipping){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if (user==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.update(user.getId(),shipping);
+    }
+
+
+    @RequestMapping("select.do")
+    @ResponseBody
+    public ServerResponse select(HttpSession session,Integer shippingId){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if (user==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.select(user.getId(),shippingId);
+    }
+
+    @RequestMapping("list.do")
+    @ResponseBody
+    public ServerResponse list(HttpSession session, @RequestParam(value = "pageNum",defaultValue = "1") Integer pageNum, @RequestParam(value = "pageSize",defaultValue = "10") Integer pageSize){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if (user==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.list(pageNum,pageSize,user.getId());
+    }
+}
